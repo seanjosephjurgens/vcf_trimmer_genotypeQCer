eefac53995905a4c28ca1fb1970f06a9c014d139
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2024-08-08 09:06:33 +0200

    Update process_vcf.sh

--- resources/home/dnanexus/process_vcf.sh
+++ resources/home/dnanexus/process_vcf.sh
@@ -32,16 +32,16 @@ then
     if [ "$2" != "NA" ] && [ "$3" != "NA" ]
     then
         export BCFTOOLS_PLUGINS=plugins/
-        bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
+        bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools annotate -x FORMAT/FT -Ou | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
         #bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
     elif [ "$2" != "NA" ] && [ "$3" == "NA" ]
     then
         export BCFTOOLS_PLUGINS=plugins/
-        bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Oz -o $FILEOUT --threads $4 -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n .
+        bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools annotate -x FORMAT/FT -Oz -o $FILEOUT --threads $4  
         #bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Oz -o $FILEINTER --threads $4
     else
         export BCFTOOLS_PLUGINS=plugins/
-        bcftools norm -m - $FILEIN -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
+        bcftools norm -m - $FILEIN -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools annotate -x FORMAT/FT -Ou | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
         #bcftools norm -m - $FILEIN -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
     fi
     #export BCFTOOLS_PLUGINS=plugins/
@@ -51,7 +51,8 @@ then
     dx upload $FILEOUT --path "$6/" --brief
 
     # 5. Clear the worker of input and output files to reserve storage
-    rm $FILEIN $FILEINTER $FILEOUT
+    #rm $FILEIN $FILEINTER $FILEOUT
+    rm $FILEIN $FILEOUT
 
 else
     echo "Ignoring VCF as no filtering or removal of data fields requested!"
