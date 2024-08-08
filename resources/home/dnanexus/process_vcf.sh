9ac7337945823cb196a67f8d73beb6b338f0d0fd
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2024-08-08 07:48:57 +0200

    Update process_vcf.sh

--- resources/home/dnanexus/process_vcf.sh
+++ resources/home/dnanexus/process_vcf.sh
@@ -31,21 +31,21 @@ then
     # 3. Run bcftools accounting for options to include
     if [ "$2" != "NA" ] && [ "$3" != "NA" ]
     then
-        #export BCFTOOLS_PLUGINS=plugins/
-        #bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
-        bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
+        export BCFTOOLS_PLUGINS=plugins/
+        bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Ob | bcftools +setGT -Ob -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
+        #bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
     elif [ "$2" != "NA" ] && [ "$3" == "NA" ]
     then
-        #export BCFTOOLS_PLUGINS=plugins/
-        #bcftools annotate -x $2 $FILEIN -Ou | bcftools norm -m - -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Oz -o $FILEOUT --threads $4 -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n .
-        bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Oz -o $FILEINTER --threads $4
+        export BCFTOOLS_PLUGINS=plugins/
+        bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Ob | bcftools +setGT -Oz -o $FILEOUT --threads $4 -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n .
+        #bcftools annotate -x $2 $FILEIN -Ob | bcftools norm -m - -Ob | bcftools view -f PASS -Oz -o $FILEINTER --threads $4
     else
-        #export BCFTOOLS_PLUGINS=plugins/
-        #bcftools norm -m - $FILEIN -Ou | bcftools view -f PASS -Ou | bcftools +setGT -Ou -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
-        bcftools norm -m - $FILEIN -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
+        export BCFTOOLS_PLUGINS=plugins/
+        bcftools norm -m - $FILEIN -Ob | bcftools view -f PASS -Ob | bcftools +setGT -Ob -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n . | bcftools filter -i $3 -Oz -o $FILEOUT --threads $4
+        #bcftools norm -m - $FILEIN -Ob | bcftools view -f PASS -Ob | bcftools filter -i $3 -Oz -o $FILEINTER --threads $4
     fi
-    export BCFTOOLS_PLUGINS=plugins/
-    bcftools +setGT $FILEINTER -Oz -o $FILEOUT --threads $4 -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n .
+    #export BCFTOOLS_PLUGINS=plugins/
+    #bcftools +setGT $FILEINTER -Oz -o $FILEOUT --threads $4 -- -t q -i 'FORMAT/FT!="PASS" & FORMAT/FT!="."' -n .
 
     # 4. Upload trimmed VCF here:
     dx upload $FILEOUT --path "$6/" --brief
